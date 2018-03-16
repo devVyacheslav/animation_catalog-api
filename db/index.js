7bc073fce9e31cbb6a71af51b7fd6b4c9d517079
storonaot <storonaot@gmail.com>
2018-03-17 00:05:51 +0300

    Some fixes

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
-const config = require('../config')
+// const config = require('../config')
 
 const mongoose = require('mongoose')
-mongoose.connect(config.get('mongoose:uri'))
+// mongoose.connect(config.get('mongoose:uri'))
+mongoose.connect(process.env.MONGODB_URI)
 
 module.exports = mongoose
