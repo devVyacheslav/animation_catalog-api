b331c01b8171445b68edea96d406e927b32970b7
storonaot <storonaot@gmail.com>
2018-03-16 23:44:45 +0300

    Add env

--- index.js
+++ index.js
@@ -1,97 +1,89 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+require('dotenv').config()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+const http = require('http')
+const bodyParser = require('body-parser')
+// const session = require('express-session')
+// const cookieParser = require('cookie-parser')
+// const checkAuth = require('./middleware/checkAuth')
+const db = require('./db')
 
-// const express = require('express')
-// const path = require('path')
-// const http = require('http')
-// const bodyParser = require('body-parser')
-// // const session = require('express-session')
-// // const cookieParser = require('cookie-parser')
-// // const checkAuth = require('./middleware/checkAuth')
-// const config = require('./config')
-// const db = require('./db')
+const PORT = process.env.PORT || 3000
+const API = '/api/v1/'
+const NODE_ENV = process.env.NODE_ENV || 'development'
 
 // const errorhandler = require('errorhandler')
 // const HttpError = require('./error').HttpError
 
-// const app = express()
-// app.set('port', config.get('port'))
+const app = express()
 
-// // Declare routes
-// const index = require('./routes')
-// const countries = require('./routes/countries')
-// const languages = require('./routes/languages')
-// const directors = require('./routes/directors')
-// const studios = require('./routes/studios')
-// const translations = require('./routes/translations')
-// const videoformats = require('./routes/videoformats')
-// const serials = require('./routes/serials')
-// const seasons = require('./routes/seasons')
-// const episodes = require('./routes/episodes')
-// const dvds = require('./routes/dvds')
-// const films = require('./routes/films')
-// const images = require('./routes/images')
+// Declare routes
+const index = require('./routes')
+const countries = require('./routes/countries')
+const languages = require('./routes/languages')
+const directors = require('./routes/directors')
+const studios = require('./routes/studios')
+const translations = require('./routes/translations')
+const videoformats = require('./routes/videoformats')
+const serials = require('./routes/serials')
+const seasons = require('./routes/seasons')
+const episodes = require('./routes/episodes')
+const dvds = require('./routes/dvds')
+const films = require('./routes/films')
+const images = require('./routes/images')
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*')
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-//   )
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-//   res.setHeader('Access-Control-Allow-Credentials', true)
-//   next()
-// })
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  )
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
 
-// app.use(bodyParser.json())
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true
-//   })
-// )
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+)
 
-// app.use('/public', express.static('public'))
+app.use('/public', express.static('public'))
 
-// // app.use(cookieParser())
-// //
-// // const MongoStore = require('connect-mongo')(session)
-// // app.use(session({
-// //   secret: config.get('session:secret'),
-// //   key: config.get('session:key'),
-// //   cookie: config.get('session:cookie'),
-// //   resave: true,
-// //   saveUninitialized: true,
-// //   store: new MongoStore({ mongooseConnection: db.connection })
-// // }))
-// //
-// const sendHttpError = require('./middleware/sendHttpError')
-// app.use(sendHttpError)
-// // app.use(require('./middleware/loadUser'))
-// //
-// const API = config.get('api')
+// app.use(cookieParser())
+//
+// const MongoStore = require('connect-mongo')(session)
+// app.use(session({
+//   secret: config.get('session:secret'),
+//   key: config.get('session:key'),
+//   cookie: config.get('session:cookie'),
+//   resave: true,
+//   saveUninitialized: true,
+//   store: new MongoStore({ mongooseConnection: db.connection })
+// }))
+//
+const sendHttpError = require('./middleware/sendHttpError')
+app.use(sendHttpError)
+// app.use(require('./middleware/loadUser'))
+//
 
-// // Init routes
-// app.use('/', index)
-// app.use(`${API}countries`, countries)
-// app.use(`${API}directors`, directors)
-// app.use(`${API}languages`, languages)
-// app.use(`${API}studios`, studios)
-// app.use(`${API}translations`, translations)
-// app.use(`${API}videoformats`, videoformats)
-// app.use(`${API}serials`, serials)
-// app.use(`${API}seasons`, seasons)
-// app.use(`${API}episodes`, episodes)
-// app.use(`${API}dvds`, dvds)
-// app.use(`${API}films`, films)
-// app.use(`${API}images`, images)
+// Init routes
+app.use('/', index)
+app.use(`${API}countries`, countries)
+app.use(`${API}directors`, directors)
+app.use(`${API}languages`, languages)
+app.use(`${API}studios`, studios)
+app.use(`${API}translations`, translations)
+app.use(`${API}videoformats`, videoformats)
+app.use(`${API}serials`, serials)
+app.use(`${API}seasons`, seasons)
+app.use(`${API}episodes`, episodes)
+app.use(`${API}dvds`, dvds)
+app.use(`${API}films`, films)
+app.use(`${API}images`, images)
 
 // app.use((err, req, res, next) => {
 //   if (typeof err === 'number') {
@@ -101,7 +93,7 @@ express()
 //   if (err instanceof HttpError) {
 //     res.sendHttpError(err)
 //   } else {
-//     if (app.get('env') === 'development') {
+//     if (NODE_ENV === 'development') {
 //       errorhandler()(err, req, res, next)
 //     } else {
 //       console.error(err)
@@ -111,9 +103,7 @@ express()
 //   }
 // })
 
-// const port = app.get('port')
-
-// const server = http.createServer(app)
-// server.listen(port, () => {
-//   console.log('Example App listening on port ' + port + '!')
-// })
+const server = http.createServer(app)
+server.listen(PORT, () => {
+  console.log('Example App listening on port ' + PORT + '!')
+})
