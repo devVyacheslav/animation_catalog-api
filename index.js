ce023b736895eb6d23f66912b27a3749de8efd11
storonaot <storonaot@gmail.com>
2018-10-26 22:51:46 +0300

    Fix origin

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const films = require('./routes/films')
 const images = require('./routes/images')
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', 'https://animation-catalog.herokuapp.com/')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
   res.setHeader('Access-Control-Allow-Credentials', true)
